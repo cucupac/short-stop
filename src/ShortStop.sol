23cfb1fd6bd87d88f88a8cd844da6c61dbf571f4
Adam Cuculich <cucupac1996@gmail.com>
2022-08-20 18:38:09 -0500

    commit before install

--- src/ShortStop.sol
+++ src/ShortStop.sol
@@ -1,9 +1,43 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// contracts/RunChallenger.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
 import "@aave-protocol/lendingpool/LendingPool.sol";
 import "@uniswap/interfaces/ISwapRouter.sol";
 import "@uniswap/libraries/TransferHelper.sol";
 
-contract ShortStop is LendingPool {}
+contract ShortStop is LendingPool {
+
+    address public shortTokenAddress;
+
+    constructor(address shortTokenAddress) {
+        shortTokenAddress = shortTokenAddress;
+    }
+   
+
+    // Handle inital deposit from user
+    function deposit() public payable {
+
+    }
+
+    // Use USD balance to initiate loan with Aave
+    function initiateLoan() private view {
+
+    }
+
+    // Swap from Short Token to USDC
+    function swapToUSDC() {
+
+    }
+
+    // Swap from USDC to ShortToken
+    function swapToShortToken() {
+
+    }
+}
+
+
+
+
+
 
