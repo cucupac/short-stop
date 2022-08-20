1b178ee3db6185c3dd37cc777e6fdacb861a8627
Adam Cuculich <cucupac1996@gmail.com>
2022-08-20 15:17:40 -0500

    Installed and imported Uniswap functionality.

--- src/ShortStop.sol
+++ src/ShortStop.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "@aave-protocol/lendingpool/LendingPool.sol";
+import "@uniswap/interfaces/ISwapRouter.sol";
+import "@uniswap/libraries/TransferHelper.sol";
 
 contract ShortStop is LendingPool {}
