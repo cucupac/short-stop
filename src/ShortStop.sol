0535263fffb6b9ad98b527853e9a61d4af36b77d
Adam Cuculich <cucupac1996@gmail.com>
2022-08-20 20:25:30 -0500

    Before aave install

--- src/ShortStop.sol
+++ src/ShortStop.sol
@@ -5,10 +5,12 @@ pragma solidity ^0.8.15;
 import "@aave-protocol/lendingpool/LendingPool.sol";
 import "@uniswap/interfaces/ISwapRouter.sol";
 import "@uniswap/libraries/TransferHelper.sol";
+import "@openzeppelin-contracts/token/ERC20/ERC20.sol";
 
-contract ShortStop is LendingPool {
+contract ShortStop is LendingPool, ERC20 {
 
     address public shortTokenAddress;
+    ERC20 usdcToken = ERC20(0x2791bca1f2de4661ed88a30c99a7a9449aa84174);
 
     constructor(address shortTokenAddress) {
         shortTokenAddress = shortTokenAddress;
@@ -16,13 +18,14 @@ contract ShortStop is LendingPool {
    
 
     // Handle inital deposit from user
-    function deposit() public payable {
-
+    function deposit(uint _amount) public payable {
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+        initiateLoan();
     }
 
     // Use USD balance to initiate loan with Aave
-    function initiateLoan() private view {
-
+    function initiateLoan(uint _amount) private view {
+        usdcToken.approve(0x794a61358D6845594F94dc1DB02A252b5b4814aD, _amount);
     }
 
     // Swap from Short Token to USDC
